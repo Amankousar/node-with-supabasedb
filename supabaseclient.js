4ff8c7e3fe9d63653ae0a766e0c62ec0a9264bf3
Amankousar <amankousar30@gmail.com>
2025-05-08 11:00:00 +0530

    Create supabaseclient.js

---
+++ supabaseclient.js
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+export { supabase }
